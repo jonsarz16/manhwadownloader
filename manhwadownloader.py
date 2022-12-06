b00aa795f7092cff74df24953bdc58037f906b71
jonsarz16 <jonsarz16@gmail.com>
2022-12-06 14:28:58 +0800

    Update manhwadownloader.py

--- manhwadownloader.py
+++ manhwadownloader.py
@@ -12,74 +12,29 @@ from selenium.webdriver import FirefoxOptions
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 browser = webdriver.Firefox(options=opts)
-
 browser.get(URL)
-st.write(browser.page_source)
 
+all_links = driver.find_elements(By.TAG_NAME, 'img')
+datalist = []
+for links in all_links:
+  x = links.get_attribute("src")
+  if "252" in x:
+    datalist.append(x)
+output_folder = "/app/manhwadownloader"
 
+for index, item in enumerate(datalist):
+  response = requests.get(item)
+  if response.status_code:
+    fp = open(f"{output_folder}/{index + 1}.png", 'wb')
+    fp.write(response.content)
+    fp.close()
 
+shutil.make_archive("trk", 'zip', output_folder)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # URL = "https://luminousscans.com/tomb-raider-king-chapter-252/"
-#
-# driver.get(URL)
-# all_links = driver.find_elements(By.TAG_NAME, 'img')
-# datalist = []
-# for links in all_links:
-#   x = links.get_attribute("src")
-#   if "252" in x:
-#     datalist.append(x)
-# output_folder = "/app/manhwadownloader"
-
-# for index, item in enumerate(datalist):
-#   response = requests.get(item)
-#   if response.status_code:
-#     fp = open(f"{output_folder}/{index + 1}.png", 'wb')
-#     fp.write(response.content)
-#     fp.close()
-
-# shutil.make_archive("trk", 'zip', output_folder)
-
-# with open("trk.zip", "rb") as file:
-#     btn = st.download_button(
-#             label="Download image",
-#             data=file,
-#             file_name="trk.zip",
-#             mime="application/zip"
-#           )
+with open("trk.zip", "rb") as file:
+    btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name="trk.zip",
+            mime="application/zip"
+          )
