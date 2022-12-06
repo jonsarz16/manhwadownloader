88f99a781fce5600d92683579a72788d93392ccf
jonsarz16 <jonsarz16@gmail.com>
2022-12-06 14:24:54 +0800

    Update manhwadownloader.py

--- manhwadownloader.py
+++ manhwadownloader.py
@@ -1,48 +1,85 @@
 import streamlit as st
-import time
+import os, sys
+
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
-import requests
-import shutil
+from selenium.webdriver import FirefoxOptions
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
 
-TIMEOUT = 2
-firefoxOptions = Options()
-firefoxOptions.add_argument("--headless")
-service = Service(GeckoDriverManager().install())
-driver = webdriver.Firefox(
-    options=firefoxOptions,
-    service=service,
-)
-# URL = "https://luminousscans.com/tomb-raider-king-chapter-252/"
-URL = st.text_input('Input URL')
-driver.get(URL)
-all_links = driver.find_elements(By.TAG_NAME, 'img')
-datalist = []
-for links in all_links:
-  x = links.get_attribute("src")
-  if "252" in x:
-    datalist.append(x)
-output_folder = "/app/manhwadownloader"
+browser.get('https://luminousscans.com/tomb-raider-king-chapter-252/')
+st.write(browser.page_source)
 
-for index, item in enumerate(datalist):
-  response = requests.get(item)
-  if response.status_code:
-    fp = open(f"{output_folder}/{index + 1}.png", 'wb')
-    fp.write(response.content)
-    fp.close()
 
-shutil.make_archive("trk", 'zip', output_folder)
 
-with open("trk.zip", "rb") as file:
-    btn = st.download_button(
-            label="Download image",
-            data=file,
-            file_name="trk.zip",
-            mime="application/zip"
-          )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # URL = "https://luminousscans.com/tomb-raider-king-chapter-252/"
+# URL = st.text_input('Input URL')
+# driver.get(URL)
+# all_links = driver.find_elements(By.TAG_NAME, 'img')
+# datalist = []
+# for links in all_links:
+#   x = links.get_attribute("src")
+#   if "252" in x:
+#     datalist.append(x)
+# output_folder = "/app/manhwadownloader"
+
+# for index, item in enumerate(datalist):
+#   response = requests.get(item)
+#   if response.status_code:
+#     fp = open(f"{output_folder}/{index + 1}.png", 'wb')
+#     fp.write(response.content)
+#     fp.close()
+
+# shutil.make_archive("trk", 'zip', output_folder)
+
+# with open("trk.zip", "rb") as file:
+#     btn = st.download_button(
+#             label="Download image",
+#             data=file,
+#             file_name="trk.zip",
+#             mime="application/zip"
+#           )
